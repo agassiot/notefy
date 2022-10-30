5ff6a750e001635878d557bd4e39965118978852
Andrew Gassiot <gassiot.andrew@gmail.com>
2022-10-30 16:13:48 -0500

    css?

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //app.use(express.static('public'));
-
+app.use(express.static(__dirname + '/public'));
 // GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname,'/public/index.html'))
